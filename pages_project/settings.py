d9aeca32ce16469b0a7683bbdfa945b8112d2bd4
praveen-stox <praveen.matlab@gmail.com>
2021-07-19 15:49:18 +0530

    New updates for Heroku Deployement

--- pages_project/settings.py
+++ pages_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-e!b0i1mr@*5i5ogrm%fk#s@oou$fe^qlx*&5-s^@oy7ce9o4p+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
